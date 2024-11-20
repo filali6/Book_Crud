ee55c9ab9e894a1046a307ddd0bea9a831e480fa
chaima filali <chaimafilali6@gmail.com>
2024-11-20 13:35:15 +0100

    utilité des option toJSON et toObject

--- models/connect.js
+++ models/connect.js
@@ -17,6 +17,13 @@ connectSchema.methods.toPublic = function () {
   return connectObject;
 };
 connectSchema.set("toJSON", { virtuals: true });
+//Lors de la conversion d'un document mongoose en json
+// par défaut les champs virtuels sont exclus
+// donc si on veut les inclure  dans l'objet json retourné
+// on doit définir l'option toJSON: { virtuals: true }
 connectSchema.set("toObject", { virtuals: true });
-
+// Lors de la conversion d'un document mongoose en objet javascript
+// par défaut les champs virtuels sont exclus
+// donc si on veut inclure les champs virtuels dans l'objet retourné
+// on doit définir l'option toObject: { virtuals: true }.
 export default mongoose.model("Connect", connectSchema);
